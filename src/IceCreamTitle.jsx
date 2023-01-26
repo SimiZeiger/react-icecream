7cc4e3b81d157ae5f49e535f9417a8555015d29c
SimiZeiger <117586967+SimiZeiger@users.noreply.github.com>
2023-01-25 20:36:01 -0500

    first commit

---
+++ src/IceCreamTitle.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css'
+
+function IceCreamTitle(props) {
+    return (
+        <article>
+            <h1>{props.title}</h1>
+            <h3 className={props.feedback.rating <= 3.5 ? 'red' : 'green'}>feedback</h3>
+        </article>
+    )
+}
+
+export default IceCreamTitle;
